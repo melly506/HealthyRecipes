8a84def03b76712887abaf66c9bc1d8fb06dea47
Melnik Valya <melnikvalya05@gmail.com>
2025-03-19 00:35:12 +0200

    add color on hover

---
+++ WebAngular/src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module'; // Додайте AppRoutingModule
+
+@NgModule({
+  declarations: [],
+  imports: [
+    BrowserModule,
+    AppRoutingModule // Додайте AppRoutingModule
+  ],
+  providers: [],
+})
+export class AppModule { }
---
+++ WebAngular/src/app/quick-recipes/quick-recipes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quick-recipes',
+  imports: [],
+  templateUrl: './quick-recipes.component.html',
+  styleUrl: './quick-recipes.component.scss'
+})
+export class QuickRecipesComponent {
+
+}
