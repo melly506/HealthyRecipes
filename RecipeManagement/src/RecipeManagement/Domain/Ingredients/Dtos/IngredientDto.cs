d0ebc0ee194c764e11cfda4c27d040610efd1887
Melnik Valya <melnikvalya05@gmail.com>
2025-03-20 07:07:47 +0200

    Return base fields for ingridients

--- RecipeManagement/src/RecipeManagement/Domain/Ingredients/Dtos/IngredientDto.cs
+++ RecipeManagement/src/RecipeManagement/Domain/Ingredients/Dtos/IngredientDto.cs
@@ -13,4 +13,8 @@ public sealed record IngredientDto
     public decimal Protein { get; set; }
     public decimal Sugar { get; set; }
 
+    public DateTimeOffset CreatedOn { get; set; }
+    public string CreatedBy { get; set; }
+    public DateTimeOffset? LastModifiedOn { get; set; }
+    public string LastModifiedBy { get; set; }
 }
