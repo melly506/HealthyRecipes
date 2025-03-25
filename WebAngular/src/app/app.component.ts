5ade92af476db43f46da501d20516ccd8bfb0dc2
Melnik Valya <melnikvalya05@gmail.com>
2025-03-26 01:22:31 +0200

    add routing profile

--- WebAngular/src/app/app.component.ts
+++ WebAngular/src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import Keycloak from 'keycloak-js';
-import { IngredientsService } from  './core/services/ingredients.service';
 
 import { HeaderComponent } from './header/header.component';
 
@@ -11,29 +9,4 @@ import { HeaderComponent } from './header/header.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  //таким чином ми підключаємо сервіс, щоб підключити його треба за import
-  #ingredients = inject(IngredientsService);
-  userInfo: any = {};
-  items: any[] = [];
-  item: any;
-  constructor(private readonly keycloak: Keycloak) {}
-
-  login() {
-    this.keycloak.login();
-  }
-
-  logout() {
-    this.keycloak.logout();
-  }
-
-  async test() {
-    this.userInfo = await this.keycloak.loadUserProfile();
-    this.#ingredients.getIngredients().subscribe(response => {
-      this.items = response;
-    });
-    this.#ingredients.getIngredientById('c6434a10-7db4-4600-b987-99d60315d039').subscribe(response =>{
-      this.item = response;
-    });
-  }
-}
+export class AppComponent {}
